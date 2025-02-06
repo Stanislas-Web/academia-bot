b8fc6e7a68c1526f136f32926f21352b93d47033
Stanislas-Web <57170716+Stanislas-Web@users.noreply.github.com>
2025-02-06 15:20:40 +0100

    update the code8r

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use("/restos", apiRoute);
+app.use("/whatsapp", apiRoute);
 
 app.listen(PORT, () => {console.log("port: " + PORT)});
